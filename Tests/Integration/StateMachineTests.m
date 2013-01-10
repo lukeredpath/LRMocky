26070ddf6aea9220a1f39b6041e71c349ebe8250
Luke Redpath <luke@lukeredpath.co.uk>
2013-01-10 12:40:10 +0000

    Use a synchroniser combined with states to test expectations that are satisfied asynchronously.

--- Tests/Integration/StateMachineTests.m
+++ Tests/Integration/StateMachineTests.m
@@ -15,6 +15,60 @@
 
 @end
 
+@interface LRSynchroniser : NSObject
+
++ (id)synchroniser;
++ (id)synchroniserWithTimeout:(NSTimeInterval)timeout;
+- (void)waitUntil:(id<LRStatePredicate>)statePredicate;
+
+@end
+
+@implementation LRSynchroniser {
+  NSTimeInterval _timeout;
+}
+
++ (id)synchroniser
+{
+  return [[self alloc] initWithTimeout:5];
+}
+
++ (id)synchroniserWithTimeout:(NSTimeInterval)timeout
+{
+  return [[self alloc] initWithTimeout:timeout];
+}
+
+- (id)initWithTimeout:(NSTimeInterval)timeout
+{
+  self = [super init];
+  if (self) {
+    _timeout = timeout;
+  }
+  return self;
+}
+
+- (void)waitUntil:(id<LRStatePredicate>)statePredicate
+{
+  if ([statePredicate isActive]) return;
+  
+  NSDate *waitUntil = [NSDate dateWithTimeIntervalSinceNow:_timeout];
+
+  BOOL predicateSatisfied = NO;
+  NSInteger pollingInterval = 0.1;
+  
+  while (!predicateSatisfied) {
+    NSDate *runUntil = [NSDate dateWithTimeIntervalSinceNow:pollingInterval];
+    
+    if ([runUntil earlierDate:waitUntil] == waitUntil) {
+      break;
+    }
+    [[NSRunLoop currentRunLoop] runUntilDate:runUntil];
+
+    predicateSatisfied = [statePredicate isActive];
+  }
+}
+
+@end
+
 DEFINE_FUNCTIONAL_TEST_CASE(StateMachineTests) {
   LRMockyStateMachine *readiness;
 }
@@ -84,6 +138,48 @@ DEFINE_FUNCTIONAL_TEST_CASE(StateMachineTests) {
   [context assertSatisfied];
   
   assertThat(testCase, passed());
+}
+
+- (void)testUsingStatesToTestAsynchronousCode
+{
+  LRSynchroniser *synchroniser = [LRSynchroniser synchroniser];
+  
+  [readiness transitionTo:@"waiting"];
+  
+  [context check:^{
+    [[expectThat(testObject) receives] doSomething]; [then state:readiness becomes:@"ready"];
+  }];
+  
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [testObject doSomething];
+  });
+  
+  [synchroniser waitUntil:[readiness equals:@"ready"]];
+  [context assertSatisfied];
+  
+  assertThat(testCase, passed());
+}
+
+- (void)testUsingStatesToTestAsynchronousCodeWithTimeout
+{
+  LRSynchroniser *synchroniser = [LRSynchroniser synchroniserWithTimeout:0.3];
+  
+  [readiness transitionTo:@"waiting"];
+  
+  [context check:^{
+    [[expectThat(testObject) receives] doSomething]; [then state:readiness becomes:@"ready"];
+  }];
+  
+  double delayInFractionalSeconds = 0.4;
+  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInFractionalSeconds * NSEC_PER_SEC);
+  dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+    [testObject doSomething];
+  });
+  
+  [synchroniser waitUntil:[readiness equals:@"ready"]];
+  [context assertSatisfied];
+  
+  assertThat(testCase, failedWithNumberOfFailures(1));
 }
 
 END_TEST_CASE
